2475e86564243354a51dd786e3d28d43cf9bb5d8
Nour Harrak <n01154202@humber.ca>
2025-04-24 02:48:03 -0400

    subclass created using inheritance

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ class productProperties {
         return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`
     }
 }
+
+class perishableProductProperties extends productProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name);
+        super(price);
+        super(quantity);
+        this.expirationDate = expirationDate;
+    }
+}
